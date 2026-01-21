ff0fa62029b6d0f1f56bc7ec92577d564687027e
csgorun <dimitryi455@gmail.com>
2026-01-21 18:52:20 +0300

    Employeeclasswithprivate fields

---
+++ src/Employee.kt
@@ -0,0 +1,6 @@
+class Employee {
+    private var fullName: String = "SDA"
+    private var position: String = "Manager"
+    private var salary: Int = 100
+    private var yearsOfExperience: Int = 1
+}
