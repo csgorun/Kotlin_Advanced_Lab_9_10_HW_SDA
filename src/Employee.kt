e1487280387f97c5b19bdd2b578657c337343b60
csgorun <dimitryi455@gmail.com>
2026-01-21 19:24:19 +0300

    Implement customsettersforsalary and experience

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,6 +1,53 @@
 class Employee {
+
     private var fullName: String = "SDA"
     private var position: String = "Manager"
-    private var salary: Int = 100
-    private var yearsOfExperience: Int = 1
+
+    var salary: Int = 100
+        private set(value) {
+            if (value < 0) {
+                println("Ошибка: зарплата не может быть отрицательной")
+            } else {
+                field = value
+            }
+        }
+
+    var yearsOfExperience: Int = 1
+        private set(value) {
+            if (value > 50) {
+                println("Опыт работы не может быть больше 50 лет")
+                field = 50
+            } else if (value < 0) {
+                println("Опыт работы не может быть меньше 0 лет")
+            } else {
+                field = value
+            }
+        }
+
+    fun updateSalary(value: Int) {
+        salary = value
+    }
+
+    fun updateYearsOfExperience(value: Int) {
+        yearsOfExperience = value
+    }
+}
+fun main() {
+
+    val test = Employee()
+
+    test.updateSalary(-10)
+    println("Зарплата: ${test.salary}")
+
+    test.updateSalary(1)
+    println("Зарплата: ${test.salary}")
+
+    test.updateYearsOfExperience(60)
+    println("Опыт: ${test.yearsOfExperience}")
+
+    test.updateYearsOfExperience(-5)
+    println("Опыт: ${test.yearsOfExperience}")
+
+    test.updateYearsOfExperience(10)
+    println("Опыт: ${test.yearsOfExperience}")
 }
